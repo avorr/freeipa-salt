63666efea7e74b1fb5be55c9809e887686521255
Александр Воробьев <apvorobev@sberbank.ru>
2023-07-03 10:48:01 +0300

    Add ability for adding and removing srv, ptr records. PLAT-24528

--- scripts/enroll.py
+++ scripts/enroll.py
@@ -1,8 +1,7 @@
-# !/usr/bin/python3
-import os
+#!/usr/bin/python3
 
+import os
 import json
-# from loguru import logger
 from python_freeipa import ClientMeta
 
 import urllib3
@@ -11,7 +10,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 def main() -> None:
-    creds: dict = {
+    ipa_cred: dict = {
         "login": os.getenv("LOGIN"),
         "password": os.getenv("PASSWORD"),
     }
@@ -19,7 +18,7 @@ def main() -> None:
     ipa_servers: list = json.loads(os.getenv("IPA_SERVERS").replace("'", "\""))
 
     client = ClientMeta(ipa_servers[0], verify_ssl=False)
-    client.login(creds["login"], creds["password"])
+    client.login(ipa_cred["login"], ipa_cred["password"])
 
     host_info: dict = {
         "dns_zone": os.getenv("IPA_DNS_ZONE"),
@@ -34,6 +33,52 @@ def main() -> None:
         "os_release": os.getenv("OS_RELEASE")
     }
 
+    def add_srv_record(dns_zone: str, srv_name: str, srv_record: str) -> None:
+        exist_srv_record: dict = client.dnsrecord_find(
+            a_dnszoneidnsname=dns_zone,
+            o_idnsname=srv_name
+        )
+
+        if not exist_srv_record["count"]:
+            client.dnsrecord_add(
+                a_dnszoneidnsname=dns_zone,
+                a_idnsname=srv_name,
+                o_srvrecord=srv_record
+            )
+
+        if exist_srv_record["result"]:
+            if srv_record not in exist_srv_record["result"][0]["srvrecord"]:
+                exist_srv_record["result"][0]["srvrecord"].append(srv_record)
+                client.dnsrecord_mod(
+                    a_dnszoneidnsname=dns_zone,
+                    a_idnsname=srv_name,
+                    o_srvrecord=exist_srv_record["result"][0]["srvrecord"]
+                )
+
+    def add_ptr_record(dns_zone: str, ptr_name: str, ptr_record: str) -> None:
+        exist_ptr_record: dict = client.dnsrecord_find(
+            a_dnszoneidnsname=dns_zone,
+            o_idnsname=ptr_name
+        )
+
+        if not exist_ptr_record["count"]:
+            client.dnsrecord_add(
+                a_dnszoneidnsname=dns_zone,
+                a_idnsname=ptr_name,
+                o_ptrrecord=ptr_record
+            )
+
+    def add_host_info() -> None:
+        client.host_mod(
+            a_fqdn=f'{host_info["vm_name"]}.{host_info["dns_zone"]}',
+            o_description=host_info["name"],
+            o_userclass=host_info["vm_type"],
+            o_nshostlocation=host_info["env"],
+            o_l=host_info["rack"],
+            o_nshardwareplatform=host_info["ipa_location"],
+            o_nsosversion=f'{host_info["os_fullname"]}-{host_info["os_release"]}'
+        )
+
     check_host: dict = client.host_find(f'{host_info["vm_name"]}.{host_info["dns_zone"]}')
 
     if not check_host["count"]:
@@ -42,54 +87,42 @@ def main() -> None:
             o_description=host_info["name"],
             o_ip_address=host_info["ip"]
         )
+        add_host_info()
 
-    if "description" not in check_host["result"][0]:
-        result: dict = client.host_mod(
-            a_fqdn=f'{host_info["vm_name"]}.{host_info["dns_zone"]}',
-            o_description=host_info["name"],
-            o_userclass=host_info["vm_type"],
-            o_nshostlocation=host_info["env"],
-            o_l=host_info["rack"],
-            o_nshardwareplatform=host_info["ipa_location"],
-            o_nsosversion=f'{host_info["os_fullname"]}-{host_info["os_release"]}'
-        )
-        # logger.info(result["summary"])
-    elif check_host["result"][0]["description"][0] != host_info["name"] or \
-            check_host["result"][0]["userclass"][0] != host_info["vm_type"] or \
-            check_host["result"][0]["nshostlocation"][0] != host_info["ipa_location"] or \
-            check_host["result"][0]["l"][0] != host_info["rack"]:
-        result: dict = client.host_mod(
-            a_fqdn=f'{host_info["vm_name"]}.{host_info["dns_zone"]}',
-            o_description=host_info["name"],
-            o_userclass=host_info["vm_type"],
-            o_nshostlocation=host_info["env"],
-            o_l=host_info["rack"],
-            o_nshardwareplatform=host_info["ipa_location"],
-            o_nsosversion=f'{host_info["os_fullname"]}-{host_info["os_release"]}'
-            # o_nsosversion=""
-        )
-        # logger.info(result["summary"])
+    check_host: dict = client.host_find(f'{host_info["vm_name"]}.{host_info["dns_zone"]}')
 
-    exist_dns_record: dict = client.dnsrecord_find(
+    attrs: dict = check_host["result"][0]
+
+    if attrs["description"][0] != host_info["name"] or attrs["userclass"][0] != host_info["vm_type"] or \
+            attrs["nshostlocation"][0] != host_info["env"] or attrs["l"][0] != host_info["rack"]:
+        add_host_info()
+
+    exist_a_record: dict = client.dnsrecord_find(
         a_dnszoneidnsname=host_info["dns_zone"],
         o_idnsname=host_info["vm_name"]
     )
 
-    if not exist_dns_record["count"]:
+    if "arecord" not in exist_a_record['result'][0]:
         client.dnsrecord_add(
             a_dnszoneidnsname=host_info["dns_zone"],
             a_idnsname=host_info["vm_name"],
             o_a_part_ip_address=host_info["ip"]
         )
 
+    add_srv_record(host_info["dns_zone"], "_blackbox_ssh._tcp", f'0 100 9022 {host_info["vm_name"]}')
+    add_srv_record(host_info["dns_zone"], "_node._tcp", f'0 100 19100 {host_info["vm_name"]}')
+    add_srv_record(host_info["dns_zone"], "_process._tcp", f'0 100 19102 {host_info["vm_name"]}')
+
+    add_ptr_record(
+        '.'.join(host_info["ip"].split('.')[0:3][::-1]) + '.in-addr.arpa.',
+        host_info["ip"].split('.')[3],
+        f'{host_info["vm_name"]}.{host_info["dns_zone"]}.'
+    )
+
     client.hostgroup_add_member(
         a_cn=f'{host_info["env"]}-{host_info["vm_type"]}',
         o_host=f'{host_info["vm_name"]}.{host_info["dns_zone"]}'
     )
-    # client.hostgroup_add_member(
-    #     a_cn=os.getenv("IPA_GROUP"),
-    #     o_host=f'{host_info["vm_name"]}.{host_info["dns_zone"]}'
-    # )
 
 
 if __name__ == '__main__':
--- scripts/unenroll.py
+++ scripts/unenroll.py
@@ -9,7 +9,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 def main() -> None:
-    creds: dict = {
+    ipa_cred: dict = {
         "login": os.getenv("IPA_LOGIN"),
         "password": os.getenv("IPA_PASSWORD"),
     }
@@ -18,13 +18,33 @@ def main() -> None:
 
     minion_id: str = os.getenv("MINION_ID")
     client = ClientMeta(ipa_servers[0], verify_ssl=False)
-    client.login(creds["login"], creds["password"])
+    client.login(ipa_cred["login"], ipa_cred["password"])
 
     check_host: dict = client.host_find(minion_id)
-    if check_host['count'] == 1:
+
+    def remove_srv_record(dns_zone: str, idnsname: str, srv_record: str):
+
+        check_srv_record: dict = client.dnsrecord_find(dns_zone, o_idnsname=idnsname)
+
+        if check_srv_record["count"] == 1:
+            if "srvrecord" in check_srv_record["result"][0]:
+                if srv_record in check_srv_record["result"][0]["srvrecord"]:
+                    srv_record_index: int = check_srv_record["result"][0]["srvrecord"].index(srv_record)
+                    check_srv_record["result"][0]["srvrecord"].pop(srv_record_index)
+                    client.dnsrecord_mod(
+                        a_dnszoneidnsname=dns_zone,
+                        a_idnsname=idnsname,
+                        o_srvrecord=check_srv_record["result"][0]["srvrecord"]
+                    )
+
+    if check_host["count"] == 1:
         client.host_del(a_fqdn=minion_id, updatedns=True)
+        hostname: str = check_host["result"][0]["serverhostname"][0]
+        dns_zone: str = minion_id[len(hostname) + 1:]
+        remove_srv_record(dns_zone, "_blackbox_ssh._tcp", f"0 100 9022 {hostname}")
+        remove_srv_record(dns_zone, "_node._tcp", f"0 100 19100 {hostname}")
+        remove_srv_record(dns_zone, "_process._tcp", f"0 100 19102 {hostname}")
 
 
 if __name__ == '__main__':
     main()
-
